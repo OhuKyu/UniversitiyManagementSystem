9ae2a1086c1f8fe29c0d5aa08542cd55a1405cff
Trần Duy <124076204+OhuKyu@users.noreply.github.com>
2024-10-19 22:11:08 +0700

    Update module-info.java

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module UniversitiyManagementSystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
-    requires java.sql;
-
-    opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
